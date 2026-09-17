b11f107622f976b3ee259fbe0c1b22b1d5b7e446
StanislavBondarenko631 <skyritm201287@gmail.com>
2026-09-17 18:03:34 +0300

    add code in app.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,11 +12,26 @@ import MovieModal from "../MovieModal/MovieModal";
 import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
 
+import ReactPaginateModule from "react-paginate";
+import type { ReactPaginateProps } from "react-paginate";
+import type { ComponentType } from "react";
+
+type ModuleWithDefault<T> = { default: T };
+
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<
+    ComponentType<ReactPaginateProps>
+  >
+).default;
+
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const [page, setPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
 
   const handleSearch = async (query: string) => {
     try {
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,14 +1,7 @@
 import axios from "axios";
-import type { Movie } from "../types/movie";
+import type { TMDBResponse } from "../types/movie";
 
 const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
-
-interface TMDBResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-}
 
 const movieInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
